1eb5ffdf94bfe11c0c49979a1e83787d5f1dfaac
Zsolt Borbély <107408415+lo-tech-man@users.noreply.github.com>
2024-05-13 14:48:48 +0200

    Refactor code, finalize project

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let computerChoice;
+let humanChoice;
+
 function getComputerChoice(min, max) {
 	let choice = Math.floor(Math.random() * (max - min + 1)) + min;
 
@@ -11,18 +14,18 @@ function getComputerChoice(min, max) {
 }
 
 function getHumanChoice() {
-	let choice = prompt("(R)ock, (p)aper, or (s)cissors? Make your choice...");
+	let choice = prompt("Rock, paper, or scissors? Make your choice...");
 
 	if (choice === null) {
 		alert("You must make a selection!");
 		return getHumanChoice();
 	} else {
 		choice = choice.toLowerCase();
-		if (choice === "r" || choice === "rock") {
+		if (choice === "rock") {
 			return "rock";
-		} else if (choice === "p" || choice === "paper") {
+		} else if (choice === "paper") {
 			return "paper";
-		} else if (choice === "s" || choice === "scissors") {
+		} else if (choice === "scissors") {
 			return "scissors";
 		} else {
 			alert("Error: Invalid choice!");
@@ -31,44 +34,38 @@ function getHumanChoice() {
 	}
 }
 
-let computerChoice;
-let humanChoice;
-
 let humanScore = 0;
 let computerScore = 0;
 
 function displayChoices() {
-	console.log(`Your pick: ${humanChoice} -- Opponent's pick: ${computerChoice}`);
+	console.log("-> Your pick: " + humanChoice + "\n-> Opponent's pick: " + computerChoice);
 }
 
 function displayScores() {
-	console.log(`Your score: ${humanScore} -- Opponent's score: ${computerScore}`);
+	console.log("-> Your score: " + humanScore + "\n-> Opponent's score: " + computerScore);
 }
 
-function showFinalResults() {
-	console.log(`--- FINAL RESULTS ---
-	You: ${humanScore}
-	Opponent: ${computerScore}`);
+function displayFinalResults() {
+	console.log("[FINAL RESULTS]\n");
+	displayScores();
 
 	if (humanScore > computerScore) {
 		console.log("Congratulations! You win!");
 	} else if (humanScore < computerScore) {
 		console.log("You lose! Better luck next time!");
 	} else {
-		console.log("Woah! It's a tie!");
+		console.log("It's a tie!");
 	}
 }
 
 function playRound(humanChoice, computerChoice) {
-	displayChoices();
-
 	switch (humanChoice) {
 		case "rock":
 			if (computerChoice === "scissors") {
-				console.log("You score! Rock beats scissors.");
+				console.log("Round win! Rock beats scissors.");
 				humanScore++;
 			} else if (computerChoice === "paper") {
-				console.log("Opponent scores! Paper beats rock.")
+				console.log("Round loss! Paper beats rock.")
 				computerScore++;
 			} else {
 				console.log("Round draw!");
@@ -77,10 +74,10 @@ function playRound(humanChoice, computerChoice) {
 
 		case "paper":
 			if (computerChoice === "rock") {
-				console.log("You score! Paper beats rock.");
+				console.log("Round win! Paper beats rock.");
 				humanScore++;
 			} else if (computerChoice === "scissors") {
-				console.log("Opponent scores! Scissors beats paper.");
+				console.log("Round loss! Scissors beats paper.");
 				computerScore++;
 			} else {
 				console.log("Round draw!");
@@ -89,23 +86,27 @@ function playRound(humanChoice, computerChoice) {
 		
 		case "scissors":
 			if (computerChoice === "paper") {
-				console.log("You score! Scissors beats paper.");
+				console.log("Round win! Scissors beats paper.");
 				humanScore++;
 			} else if (computerChoice === "rock") {
-				console.log("Opponent scores! Rock beats scissors.");
+				console.log("Round loss! Rock beats scissors.");
 				computerScore++;
 			} else {
 				console.log("Round draw!");
 			}
 			break;
 	}
-	displayScores();
 }
 
-for (let i = 0; i < 5; i++) {
-	computerChoice = getComputerChoice(0, 2);
-	humanChoice = getHumanChoice();
-	playRound(humanChoice, computerChoice);
+function playGame() {
+	for (let i = 0; i < 5; i++) {
+		computerChoice = getComputerChoice(0, 2);
+		humanChoice = getHumanChoice();
+		displayChoices();
+		playRound(humanChoice, computerChoice);
+		displayScores();
+	}
 }
 
-showFinalResults();
+playGame();
+displayFinalResults();
